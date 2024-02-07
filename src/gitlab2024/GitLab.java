bf6bf7a1a6daa91f3cf3dd28b67e46f6e53884e2
SirFatredVIII <dhall25@huskers.unl.edu>
2024-02-07 11:04:10 -0600

    next-commit

--- src/gitlab2024/GitLab.java
+++ src/gitlab2024/GitLab.java
@@ -3,7 +3,8 @@ package gitlab2024;
 public class GitLab {
     
     public static void main(String args) {
-        System.out.println("This is a lesson on Git!");
+        GitLabHelper helper = new GitLabHelper("Name1", "Name2");
+        helper.PrintWelcomeMessage();
     }
     
 }
