3ea7c28f091d3faf2885df1b8e9e32bd38afc890
SirFatredVIII <dhall25@huskers.unl.edu>
2024-02-07 11:06:41 -0600

    third-commit

--- src/gitlab2024/GitLab.java
+++ src/gitlab2024/GitLab.java
@@ -2,9 +2,10 @@ package gitlab2024;
 
 public class GitLab {
     
-    public static void main(String args) {
-        GitLabHelper helper = new GitLabHelper("Name1", "Name2");
+    public static void main(String[] args) {
+        GitLabHelper helper = new GitLabHelper(new String[] {"Name1",
+        "Name2"});
         helper.PrintWelcomeMessage();
-    }
+        }
     
 }
