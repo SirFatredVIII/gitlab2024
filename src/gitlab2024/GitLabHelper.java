c78f6015e7efe5245c04f63ef672a6cf560deb01
SirFatredVIII <dhall25@huskers.unl.edu>
2024-02-07 11:08:42 -0600

    fourth-commit

--- src/gitlab2024/GitLabHelper.java
+++ src/gitlab2024/GitLabHelper.java
@@ -1,11 +1,12 @@
 package gitlab2024;
-import java.util.Arrays;
 public class GitLabHelper {
-private String[] persons;
-GitLabHelper(String[] persons) {
-this.persons = persons;
+private String person1;
+private String person2;
+GitLabHelper(String person1, String person2) {
+this.person1 = person1;
+this.person2 = person2;
 }
 public void PrintWelcomeMessage() {
-System.out.print("Git lab by: " + Arrays.toString(persons));
+System.out.printf("Git lab by: %s and %s", person1, person2);
 }
 }
